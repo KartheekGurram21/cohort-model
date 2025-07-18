2cd2fc649ad4336e900d7751de488b78957c0288
KartheekGurram21 <techloom.web3@gmail.com>
2025-07-18 20:08:06 +0530

    added admin router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { userRouter } = require('./routes/userRoutes');
+const { courseRouter } = require('./routes/courseRoutes');
+const { adminRouter } = require('./routes/adminRoutes');
 const app = express();
 
-app.use('/user', userRouter);
-
+app.use('/api/user', userRouter);
+app.use('/api/course', courseRouter);
+app.use('/api/admin', adminRouter);
 
 
 app.listen(3000);
